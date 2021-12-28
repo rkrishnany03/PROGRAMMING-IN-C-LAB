011509d33534a502c3cb6b3f6e569afc9e8567d4
rkrishnany03 <96784820+rkrishnany03@users.noreply.github.com>
2021-12-28 21:47:10 +0530

    Add files via upload

---
+++ PATTERN3.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,n,a;
+	clrscr();
+	printf("enter the no. of rows ");
+	scanf("%d",&n);
+	a=n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=a;j++)
+		{
+			printf("*\t");
+		}
+		printf("\n");
+		a--;
+	}
+	getch();
+}
