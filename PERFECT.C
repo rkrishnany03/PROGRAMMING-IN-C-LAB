ea9b9786018f6713c2a71ee2d03245e4fe779a89
rkrishnany03 <96784820+rkrishnany03@users.noreply.github.com>
2021-12-29 21:25:44 +0530

    Add files via upload

---
+++ PERFECT.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,n,sum=0;
+	clrscr();
+	printf("enter the nuber ");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			sum=sum+i;
+		}
+	}
+		if(sum==n)
+		{
+			printf("%d is a perfect number ",n);
+		}else
+		{
+			printf("%d is not a perfect number ",n);
+		}
+	getch();
+
+}
